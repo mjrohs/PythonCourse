300665cb7fdc2211b1e5d74a37adf2a7ec81049f
Melissa Rohs <mjrohs323@gmail.com>
2022-02-05 14:54:55 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
+# Design a program that asks the user to enter a series of 20 numbers. The program should store the numbers in a list
+# then display:
+# lowest num
+# highest num
+# total
+# average
+
+# constant variable for number of elements allowed in the list
+LIST_CONTENTS = 20
+
+
 def main():
-    # open file
-    read_file = open('names.txt', 'r')
-    # create counter for names
-    name_count = 0
+    my_list = get_list()
+    print(my_list)
+    # prints smallest number in list to console
+    print('The lowest number in the list is:', min(my_list))
+    # prints largest number in the list to the console
+    print('The highest number in the list is:', max(my_list))
+    print('The total of the entered numbers is:', total_list(my_list))
+    print('The average of the entered numbers is:', format(average_list(my_list), '.2f'))
 
-    # iterate through file and add to counter for each line
-    for line in read_file:
-        name_count += 1
 
-    # close file
-    read_file.close()
+# fill list with user-generated data
+def get_list():
+    user_list = []
+    i = 0
+    # use constant to determine number of iterations
+    while i < LIST_CONTENTS:
+        try:
+            number = int(input('Enter a number:'))
+            user_list.append(number)
+            i += 1
+        # if user enters non-integer type, output error message
+        except ValueError:
+            print('Invalid entry. You must enter a whole number.')
 
-    # output final total of names
-    print('There are', name_count, 'names in this file')
+    return user_list
+
+
+# adds all numbers in list together and returns the sum
+def total_list(user_list):
+    total = 0
+    for number in user_list:
+        total += number
+    return total
+
+
+# uses the sum from total_list() to find and return the average
+def average_list(user_list):
+    total = total_list(user_list)
+    average = total / LIST_CONTENTS
+    return average
 
 
 main()
